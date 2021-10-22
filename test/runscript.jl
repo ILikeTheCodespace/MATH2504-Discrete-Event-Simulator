1f56e37fdd83d30f9cde993e198081bf1aa51c3e
ILikeTheCode <79585703+ILikeTheCode@users.noreply.github.com>
2021-10-22 16:00:12 +1000

    Remove runscript.jl, replaced with runscript_2.jl

--- test/runscript.jl
+++ test/runscript.jl
@@ -1,92 +1,116 @@
 include("../src/dependencies.jl")
-# include("src/dependencies.jl") # This line is just here to test the plot function call at the end of the file from the Julia REPL.
 
-lambda_range = [i for i in 0.2:0.2:1.2]
-
-# Simulation recorded data
 sojurn_times = Float64[]
-system_job_totals, total_orbiting_jobs = Int64[], Int64[]
 
-# Processed simulation data for project plots.
-mean_system_job_totals = zeros(Float64, length(lambda_range))
-proportion_orbiting_jobs = zeros(Float64, length(lambda_range))
+number_in_system_integral = 0
+prev_number_in_system = 0
+prev_time = 0
+
 processed_sojurn_times = []
 
-# function plot_data_callbacks(time::Float64, state::NetworkState, event::TimedEvent)
-#     state.number_in_system_decreased && push!(sojurn_times, time - event.arrival_time)
-#     push!(total_orbiting_jobs, state.orbiting_jobs)
-#     push!(system_job_totals, state.number_in_system)
-#     return nothing
-# end
 
-prev_time = 0
-prev_number_in_system = 0
-number_in_system_integral = 0.0
-
-function plot_data_callbacks(time::Float64, state::NetworkState, event::TimedEvent)
-    state.number_in_system_decreased && push!(sojurn_times, time - event.arrival_time)
+function mode_1(time::Float64, state::NetworkState, event::TimedEvent)
     global number_in_system_integral += (time - prev_time) * prev_number_in_system
     global prev_number_in_system = state.number_in_system
     global prev_time = time
     return nothing
 end
 
-"""
-Call simulate function below for lambdas in lambda_range
-"""
-
-#TODO: This whole loop is pretty gross but was fast to make and gets the job done, consider refactoring for cleanliness
-for i in 1:length(lambda_range)
-    current_scenario = NetworkParameters(  L=5, 
-                                gamma_shape = 3.0, 
-                                λ = i, 
-                                η = 4.0, 
-                                μ_vector = collect(5:-1:1),
-                                P = [0   0.5 0.5 0   0;
-                                     0   0   0   1   0;
-                                     0   0   0   0   1;
-                                     0.5 0   0   0   0;
-                                     0.2 0.2 0.2 0.2 0.2],
-                                Q = [0 0 0 0 0;
-                                     1 0 0 0 0;
-                                     1 0 0 0 0;
-                                     1 0 0 0 0;
-                                     1 0 0 0 0],                             
-                                p_e = AnalyticWeights([0.2, 0.2, 0, 0, 0.6]),
-                                K = [-1, -1, 10, 10, 10])
-
-    global sojurn_times, total_orbiting_jobs, system_job_totals, prev_time, prev_number_in_system, number_in_system_integral
-
-    Random.seed!(1)
-
-    println("Starting simulation for λ = ", lambda_range[i])
-    
-    @time sim_states = simulate(NetworkState(0, zeros(Int8, current_scenario.L), 0), TimedEvent(ArrivalEvent(),0.0, 0, 0.0), current_scenario, max_time = 100000000.0, callback = plot_data_callbacks)
-
-    println("Simulation for lambda = ", lambda_range[i], " complete. Processing data for lambda = ", lambda_range[i], " now.")
-
-    # Logic to generate the data needed for Plot 1.
-    mean_system_job_totals[i] = number_in_system_integral/100000000.0
-    # system_job_totals_length = length(system_job_totals)
-    # for j in system_job_totals
-    #     mean_system_job_totals[i] += j/system_job_totals_length
-    # end
-
-    # Logic to generate the data needed for Plot 2.
-    proportion_orbiting_jobs[i] = sim_states.orbiting_jobs/sim_states.number_in_system
-    # proportion_orbiting_jobs[i] = last(total_orbiting_jobs)/last(system_job_totals)
-
-    # Logic to generate the data needed for Plot 3.
-    push!(processed_sojurn_times, sojurn_times)
-
-    println("Data for lambda = ", lambda_range[i], " has now been processed.\n")
-
-    sojurn_times = Float64[]
-    system_job_totals, total_orbiting_jobs = Int64[], Int64[]
-    
-    prev_time = 0
-    prev_number_in_system = 0
-    number_in_system_integral = 0
+function mode_2(time::Float64, state::NetworkState, event::TimedEvent)
+    state.number_in_system_decreased && push!(sojurn_times, time - event.arrival_time)
+    return nothing
 end
 
-scenario_plots(processed_sojurn_times, mean_system_job_totals, proportion_orbiting_jobs, 999, lambda_range)
+modes_dict = Dict(1 => mode_1, 2 => mode_2)
+
+function run_script(params::NetworkParameters; max_time::Float64, lambda_range::Vector{Float64}, mode::Int=1, plot_num::Int=1)
+
+    global modes_dict
+    
+    plot_data_callbacks = modes_dict[mode]
+
+    mean_system_job_totals = zeros(Float64, length(lambda_range))
+    proportion_orbiting_jobs = zeros(Float64, length(lambda_range))
+
+    for i in 1:length(lambda_range)
+
+        global sojurn_times, prev_time, prev_number_in_system, number_in_system_integral, processed_sojurn_times
+
+        params.λ = lambda_range[i]
+    
+        Random.seed!(1)
+    
+        println("Starting simulation for λ = ", lambda_range[i])
+        
+        sim_states = simulate(NetworkState(0, zeros(Int8, params.L), 0), TimedEvent(ArrivalEvent(),0.0, 0, 0.0), params, max_time = max_time, callback = plot_data_callbacks)
+    
+        println("Simulation for lambda = ", lambda_range[i], " complete. Processing data for lambda = ", lambda_range[i], " now.")
+    
+        if mode == 1
+            # Logic to generate the data needed for Plot 1.
+            mean_system_job_totals[i] = number_in_system_integral/max_time
+
+            # Logic to generate the data needed for Plot 2.
+            proportion_data = sim_states.orbiting_jobs/sim_states.number_in_system
+            !isnan(proportion_data) ? proportion_orbiting_jobs[i] = proportion_data : proportion_orbiting_jobs[i] = 0.0
+            
+            # proportion_orbiting_jobs[i] = sim_states.orbiting_jobs/sim_states.number_in_system == NaN ? 0.0 : sim_states.orbiting_jobs/sim_states.number_in_system
+
+        elseif mode == 2
+            # Logic to generate the data needed for Plot 3.
+            push!(processed_sojurn_times, sojurn_times)
+        else
+            throw(DomainError(mode, "Mode number currently unsupported."))
+        end
+
+        println("Data for lambda = ", lambda_range[i], " has now been processed.\n")
+    
+        sojurn_times = Float64[]
+        prev_time = 0
+        prev_number_in_system = 0
+        number_in_system_integral = 0
+    end
+    scenario_plots(processed_sojurn_times, mean_system_job_totals, proportion_orbiting_jobs, plot_num, lambda_range, mode)
+end
+
+#################################
+### NetworkParameters Outline ###
+#################################
+
+"""
+@with_kw mutable struct NetworkParameters
+    L::Int #amount of stations
+    gamma_shape::Float64 #external rate of arrivals
+    λ::Float64 #This is undefined for the scenarios since it is varied
+    η::Float64 #overflow movement rate
+    μ_vector::Vector{Float64} #service rates
+    P::Matrix{Float64} #routing matrix
+    Q::Matrix{Float64} #overflow matrix
+    p_e::AbstractWeights #external arrival distribution
+    K::Vector{Int} #-1 means infinity 
+end
+"""
+
+###############################
+### HOW TO USE THIS PROGRAM ###
+###############################
+
+"""
+Please follow the instructions to README.md which explains how to use this program. The following variables test_params and lambdas along with the subsequent call to run_script are all the inputs needed to run this program. Once the inputs are what you want to them to be, run this script without debugging in VSCode with Ctrl+F5 and wait for the script to finish processing the request. Once finished, the script will output PNG files related to the mode that you have selected.
+"""
+
+test_params = NetworkParameters(  L=3, 
+                                gamma_shape = 3.0, 
+                                λ = NaN, 
+                                η = 4.0, 
+                                μ_vector = ones(3),
+                                P = [0 1.0 0;
+                                    0 0 1.0;
+                                    0.5 0 0],
+                                Q = zeros(3,3),
+                                p_e = AnalyticWeights([1.0, 0, 0]),
+                                K = fill(5,3))
+
+lambdas = [i for i in 0.2:0.2:0.8] 
+
+run_script(test_params, max_time = 10000.0, lambda_range = lambdas, mode=1, plot_num=102)
